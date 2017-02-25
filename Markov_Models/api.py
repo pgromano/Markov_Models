1905757a3b3929f20fe715bf74234972810dcb2f
pgromano <pablo.romano42@gmail.com>
2017-02-25 12:09:26 -0800

    First commit

--- Markov_Models/api.py
+++ Markov_Models/api.py
@@ -27,15 +27,14 @@ class Markov_Chain(object):
         self.n_samples = [self.data[i].shape[0] for i in range(self.n_sets)]
         self.n_features = [self.data[i].shape[1] for i in range(self.n_sets)]
 
-from Markov_Models import util
 class Load(object):
     def __init__(self, files):
         self.files = files
 
     @property
     def from_CSV(self):
-        return util.load.from_CSV(self.files)
+        return mm.util.load.from_CSV(self.files)
 
     @property
     def from_ASCII(self):
-        return util.load.from_ASCII(self.files)
+        return mm.util.load.from_ASCII(self.files)
