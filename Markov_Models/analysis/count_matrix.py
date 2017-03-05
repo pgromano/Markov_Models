1814ee66392e9dc86b30463bb00ac38b4d1b2b33
pgromano <pablo.romano42@gmail.com>
2017-03-04 16:22:58 -0800

    fixed bug in finding number of states

--- Markov_Models/analysis/count_matrix.py
+++ Markov_Models/analysis/count_matrix.py
@@ -17,8 +17,8 @@ def _count_matrix(dtraj, lag, nstates, sparse):
         return C.toarray()
 
 def count_matrix(dtraj, lag=1, sparse=False):
-    nstates = np.amax(dtraj)+1
     nsets = len(dtraj)
+    nstates = np.amax([dtraj[i] for i in range(nsets)])+1
 
     C = [_count_matrix(dtraj[i], lag, nstates, sparse) for i in range(nsets)]
     return np.sum(C, axis=0)
