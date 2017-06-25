5bbfaa3df8405c5f8e81b5a0ab8cfffb542fcdfa
pgromano <pablo.romano42@gmail.com>
2017-06-24 20:25:17 -0700

    Added Hierarchical Clustering, coarse graining method

--- Markov_Models/analysis/coarse_grain.py
+++ Markov_Models/analysis/coarse_grain.py
@@ -1,7 +1,9 @@
+from . import src
+
 import numpy as np
 import copy, warnings
 from msmtools.analysis.api import _pcca_object as _PCCA
-from . import src
+from sklearn.preprocessing import MinMaxScaler as _MinMaxScaler
 
 def PCCA(self, n_macrostates, lag=None):
     # Function to convert microstate trajectory to macrostate assignments
@@ -40,9 +42,29 @@ def HMM(self, n_macrostates):
         hmm = _HMM(n_components=n_macrostates).fit(self._micro.centroids)
         self.labels = [hmm.predict(self._base.data[i]) for i in range(self._base.n_sets)]
         self.metastable_labels = hmm.predict(self._micro.centroids)
+        self.metastable_sets = []
+        for i in range(self._N):
+            self.metastable_sets.append(np.where(hmm.labels_ == i)[0])
 
 from sklearn.mixture import GaussianMixture as _GMM
 def GMM(self, n_macrostates):
     gmm = _GMM(n_components=n_macrostates).fit(self._micro.centroids)
     self.labels = [gmm.predict(self._base.data[i]) for i in range(self._base.n_sets)]
     self.metastable_labels = gmm.predict(self._micro.centroids)
+    self.metastable_covariances = gmm.covariances_
+    self.aic = gmm.aic
+    self.bic = gmm.bic
+    self.metastable_sets = []
+    for i in range(self._N):
+        self.metastable_sets.append(np.where(gmm.labels_ == i)[0])
+
+from sklearn.cluster import AgglomerativeClustering as _HC
+def HC(self, n_macrostates):
+    scaler = _MinMaxScaler(feature_range=(0,1)).fit(self._micro.centroids)
+    hc = _HC(n_clusters=n_macrostates).fit(scaler.transform(self._micro.centroids))
+
+    self.labels = [hc.predict(scaler.transform(self._base.data[i])) for i in range(self._base.n_sets)]
+    self.metastable_labels = hc.labels_
+    self.metastable_sets = []
+    for i in range(self._N):
+        self.metastable_sets.append(np.where(hc.labels_ == i)[0])
