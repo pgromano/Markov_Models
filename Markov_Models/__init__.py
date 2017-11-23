780c867eb077c8206b96586cfe646427c1db0b9b
pgromano <pablo.romano42@gmail.com>
2017-11-22 18:01:24 -0800

    Removed unnecessary files

--- Markov_Models/__init__.py
+++
@@ -1,2 +0,0 @@
-from .api import MSM, Markov_Chain
-from . import analysis, base, cluster, load, mixture, msm, splitter, util
--- Markov_Models/cluster/__init__.py
+++
@@ -1,9 +0,0 @@
-from .AffinityPropagation import _AffinityPropagation as AffinityPropagation
-from .AgglomerativeClustering import _AgglomerativeClustering as AgglomerativeClustering
-from .Birch import _Birch as Birch
-from .KMeans import _KMeans as KMeans
-from .MiniBatchKMeans import _MiniBatchKMeans as MiniBatchKMeans
-from .HMM import _HiddenMarkovModels as HMM
-from .KMedoids import _KMedoids as KMedoids
-
-from . import src
